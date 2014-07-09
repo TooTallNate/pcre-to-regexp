95395e73d4225720d36e58ddf6e8bc27e3b5f169
Nathan Rajlich <nathan@tootallnate.net>
2014-07-09 13:59:11 -0700

    test: move "keys" tests into their own test cases

--- test/test.js
+++ test/test.js
@@ -25,15 +25,18 @@ describe('PCRE(pattern[, flags])', function () {
 
   describe('given "%^https?:\/\/twitter\\.com(\/\\#\\!)?\/(?P<username>[a-zA-Z0-9_]{1,20})\\\/status(es)?\/(?P<id>\\d+)\/?$%i"', function () {
 
-    it('should match "https://twitter.com/tootallnate/status/481604870626349056"', function () {
-      var url = 'https://twitter.com/tootallnate/status/481604870626349056';
+    it('should have [ , "username", , "id" ] for keys', function () {
       var keys = [];
       var re = PCRE("%^https?:\/\/twitter\\.com(\/\\#\\!)?\/(?P<username>[a-zA-Z0-9_]{1,20})\\\/status(es)?\/(?P<id>\\d+)\/?$%i", keys);
       var expectedKeys = new Array(4);
       expectedKeys[1] = 'username';
       expectedKeys[3] = 'id';
       assert.deepEqual(keys, expectedKeys);
-      var match = re.exec(url);
+    });
+
+    it('should match "https://twitter.com/tootallnate/status/481604870626349056"', function () {
+      var re = PCRE("%^https?:\/\/twitter\\.com(\/\\#\\!)?\/(?P<username>[a-zA-Z0-9_]{1,20})\\\/status(es)?\/(?P<id>\\d+)\/?$%i");
+      var match = re.exec('https://twitter.com/tootallnate/status/481604870626349056');
       assert(match);
       assert(match[2] === 'tootallnate');
       assert(match[4] === '481604870626349056');
@@ -44,11 +47,16 @@ describe('PCRE(pattern[, flags])', function () {
   // PHP 5.2.2-style "alternative" named capture stynax using single quotes
   describe('given "#(?\'name\'foo)bar#"', function () {
 
-    it('should match "foobar"', function () {
+    it('should have [ "name" ] for keys', function () {
       var keys = [];
       var re = PCRE("#(?\'name\'foo)bar#", keys);
       assert.deepEqual(keys, ['name']);
+    });
+
+    it('should match "foobar"', function () {
+      var re = PCRE("#(?\'name\'foo)bar#");
       var match = re.exec('foobar');
+      assert(match);
       assert(match[1] === 'foo');
     });
 
@@ -57,10 +65,14 @@ describe('PCRE(pattern[, flags])', function () {
   // PHP 5.2.2-style "alternative" named capture stynax using angle brackets
   describe('given "#(?<name>foo)bar#"', function () {
 
-    it('should match "foobar"', function () {
+    it('should have [ "name" ] for keys', function () {
       var keys = [];
-      var re = PCRE("#(?<name>foo)bar#", keys);
+      var re = PCRE("#(?\'name\'foo)bar#", keys);
       assert.deepEqual(keys, ['name']);
+    });
+
+    it('should match "foobar"', function () {
+      var re = PCRE("#(?<name>foo)bar#");
       var match = re.exec('foobar');
       assert(match);
       assert(match[1] === 'foo');
